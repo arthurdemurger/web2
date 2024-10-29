91d24535a490f3003db390b9f6c69f5c531699bc
lirockk <98538729+lirockk@users.noreply.github.com>
2024-10-29 16:09:00 +0100

    new:ex2.1 & ex2.2

--- tutorials/components/src/App.tsx
+++ tutorials/components/src/App.tsx
@@ -1,45 +1,61 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import sound from "./assets/sounds/Infecticide-11-Pizza-Spinoza.mp3"
-import logo from "./assets/img/js-logo.png";
-import pizzaBackground from "./assets/img/pizza.jpg";
+import logo from "./assets/images/js-logo.png";
 
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div style={{ backgroundImage: `url(${pizzaBackground})` }}>
-      <header>
-            <h1 className="animate__animated animate__bounce">We love Pizza</h1>
-          </header>
-
-          <main>
-            <p>My HomePage</p>
-            <p>
-              Because we love JS, you can also click on the header to stop / start the
-              music ; )
-            </p>
-
-            <audio id="audioPlayer" controls autoPlay>
-              <source
-                src={sound}
-                type="audio/mpeg"
-              />
-              Your browser does not support the audio element.
-            </audio>
-          </main>
-
-          <footer>
-            <h1 className="animate__animated animate__bounce animate__delay-2s">
-              But we also love JS
-            </h1>
-            <img src={logo} alt="" />
-          </footer>
-	</div>
-  );
+	return (
+	  <div className="page">
+		<Header title="We love Pizza" version={0+1}/>
+		<Main />
+		<Footer />
+	  </div>
+	);
 }
 
+
+interface HeaderProps {
+	title: string;
+	version:number;
+}
+
+const Header = (props: HeaderProps) => {
+	return (
+	<header>
+		<h1 className="animate__animated animate__bounce">{props.title}</h1>
+		<h4>Version: {props.version}</h4>
+	</header>
+	);
+};
+
+
+const Main = () => {
+	return (
+	<main>
+		<p>My HomePage</p>
+		<p>
+		Because we love JS, you can also click on the header to stop / start the
+		music ; )
+		</p>
+		<audio id="audioPlayer" controls autoPlay>
+		<source src={sound} type="audio/mpeg" />
+		Your browser does not support the audio element.
+		</audio>
+	</main>
+	);
+};
+
+const Footer = () => {
+	return (
+	<footer>
+		<h1 className="animate__animated animate__bounce animate__delay-2s">
+		But we also love JS
+		</h1>
+		<img src={logo} alt="" />
+	</footer>
+	);
+};
+
 export default App;
+
