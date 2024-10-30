c0d2e940bc60e6c8ef408f41bd4aac0587a3bc39
lirockk <98538729+lirockk@users.noreply.github.com>
2024-10-30 17:14:55 +0100

    new: ex2.5-destructuring

--- exercises/2.5/src/components/ClickCounter/index.tsx
+++ exercises/2.5/src/components/ClickCounter/index.tsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 
+interface ClickCounterProps {
+	title: string;
+	message: string;
+}
 
-const ClickCounter = () => {
+const ClickCounter = ({title, message}: ClickCounterProps) => {
 	const [count, setCount] = useState(0);
 
 	return (
 		<div className="card">
+			<p>{title}</p>
 			<button onClick={() => setCount((count) => count + 1)}>
 				count is {count}
+				{count >= 10 && <span><br/><br/>{message}</span>}
 			</button>
 		</div>
 );
